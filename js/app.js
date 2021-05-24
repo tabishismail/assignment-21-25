1cd4005effa59cf8c5bef3bdc730ae7ff670ad8c
tabishismail <tabish554@gmail.com>
2021-05-24 22:42:31 +0500

    Assignment # 2 Done

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Chapter 21 to 25 Assignments
+
+// Assignment # 1 
+
+// var user1 = prompt ("Enter Your First Name");
+// var user2 = prompt ("Enter Your Last Name");
+// var fullName = user1 +" "+ user2;
+
+// alert ("Hello "+fullName+ " Welcome to world for Developing website" );
+
+// Assignment # 2
+
+var user = prompt("Enter Your Favourite Mobile Model");
+
+document.write("<div>"+"My Favourite Mobile model is : "+ user+"</div>");
+document.write("<div>"+"Length of string :" + user.length +"</div>");
